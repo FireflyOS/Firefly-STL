87f4c6cf91b5594b5c6525feaa992c45e1cd927b
Yuhanun Citgez <yuhanun.c@gmail.com>
2019-10-05 01:04:09 +0200

    Added nullptr_t and std::optional

--- optional.h
+++ optional.h
@@ -2,73 +2,83 @@
 #include "cstdlib/cstddef.h"
 #include "utility.h"
 
-template <typename T>
-class optional {
-    bool _ok;
-    T _val;
+namespace firefly::std {
+    template <typename T>
+    class optional {
+        bool _ok;
+        T _val;
 
-public:
-    constexpr optional()
-        : _ok{ false } {
-    }
+    public:
+        constexpr optional(T&& val) 
+            : _ok{ true }, _val{ val } {
+        }
 
-    constexpr optional(nullptr_t)
-        : _ok{ false } {
-    }
+        constexpr optional(T const& val)
+            : _ok{ true }, _val{ val } {
+        }
 
-    constexpr optional(const optional<T>& other)
-        : _ok{ other._ok }, _val{ other._val } {
-    }
+        constexpr optional()
+            : _ok{ false } {
+        }
 
-    constexpr optional(optional<T>&& other)
-        : _ok{ other._ok }, _val{ firefly::std::move(other._val) } {
-    }
+        constexpr optional(nullptr_t)
+            : _ok{ false } {
+        }
 
-    template <typename U>
-    constexpr optional(const optional<U>& other)
-        : _ok{ other._ok }, _val{ other._val } {
-    }
+        constexpr optional(const optional<T>& other)
+            : _ok{ other._ok }, _val{ other._val } {
+        }
 
-    template <typename U>
-    constexpr optional(optional<U>&& other)
-        : _ok{ other._ok }, _val{ firefly::std::move(other._val) } {
-    }
+        constexpr optional(optional<T>&& other)
+            : _ok{ other._ok }, _val{ firefly::std::move(other._val) } {
+        }
 
-    optional& operator=(optional const&) = default;
-    optional& operator=(operator&&) = default;
-    ~optional() = default;
+        template <typename U>
+        constexpr optional(const optional<U>& other)
+            : _ok{ other._ok }, _val{ other._val } {
+        }
 
-    T* operator->() {
-        return &_val;
-    }
+        template <typename U>
+        constexpr optional(optional<U>&& other)
+            : _ok{ other._ok }, _val{ firefly::std::move(other._val) } {
+        }
 
-    T& operator*() {
-        return _val;
-    }
+        optional& operator=(optional const&) = default;
+        optional& operator=(operator&&) = default;
+        ~optional() = default;
 
-    operator bool() {
-        return _ok;
-    }
+        T* operator->() {
+            return &_val;
+        }
 
-    bool has_value() {
-        return _ok;
-    }
-
-    T& value() {
-        return _val;
-    }
-
-    T& value_or(T& val) {
-        if (has_value()) {
+        T& operator*() {
             return _val;
         }
-        return val;
-    }
 
-    T value_or(T val) {
-        if (has_value()) {
+        operator bool() {
+            return _ok;
+        }
+
+        bool has_value() {
+            return _ok;
+        }
+
+        T& value() {
             return _val;
         }
-        return val;
-    }
-};
+
+        T& value_or(T& val) {
+            if (has_value()) {
+                return _val;
+            }
+            return val;
+        }
+
+        T value_or(T val) {
+            if (has_value()) {
+                return _val;
+            }
+            return val;
+        }
+    };
+} // namespace firefly::std
