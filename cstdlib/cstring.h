d10a2c9c8c35ee86a06e26bf57f5fd549f6b7b84
Yuhanun Citgez <y.citgez@student.utwente.nl>
2019-12-24 19:43:09 +0100

    Added some stuff for io

--- cstdlib/cstring.h
+++ cstdlib/cstring.h
@@ -12,6 +12,14 @@ inline size_t strnlen(const char *str) {
     return strlen(str) + 1;
 }
 
+extern void *memset(void *s, char c, int n);
+extern int memcmp(const char *s1, const char *s2, int n);
+extern char *strcpy(char *dest, const char *src);
+extern int strcmp(const char *s1, const char *s2);
+extern char *strchr(const char *str, int c);
+extern char *strchrn(const char *str, int c, int n);
+extern int toupper(char c);
+extern char *strtok(char *s, const char *delimiters);
 
 extern "C" void memcpy(void* dest, const void* src, size_t count);
 
