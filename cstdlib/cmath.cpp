c6609a90e8ea090ba702a8356477948b7fd288de
Yuhanun Citgez <yuhanun.c@gmail.com>
2021-08-08 13:45:09 +0200

    Added ln + log10 and resolved some issues with sqrt

--- cstdlib/cmath.cpp
+++ cstdlib/cmath.cpp
@@ -1,17 +1,23 @@
 #include "cmath.h"
 
-// Method 3
+// Method 1
 // https://www.codeproject.com/Articles/69941/Best-Square-Root-Method-Algorithm-Function-Precisi
 // https://stackoverflow.com/questions/5000109/implement-double-sqrtdouble-x-in-c
 float sqrt(float arg) {
     union {
         int i;
-        float exp;
+        float x;
     } u;
-
-    u.exp = arg;
+    u.x = arg;
     u.i = (1 << 29) + (u.i >> 1) - (1 << 22);
-    return u.exp;
+
+    // Two Babylonian Steps (simplified from:)
+    // u.x = 0.5f * (u.x + x/u.x);
+    // u.x = 0.5f * (u.x + x/u.x);
+    u.x = u.x + x / u.x;
+    u.x = 0.25f * u.x + x / u.x;
+
+    return u.x;
 }
 
 // https://stackoverflow.com/questions/26860574/pow-implementation-in-cmath-and-efficient-replacement
@@ -46,3 +52,24 @@ double pow(double base, int exponent) {
             return (temp * temp) / base;
     }
 }
+
+// https://stackoverflow.com/questions/35968963/trying-to-calculate-logarithm-base-10-without-math-h-really-close-just-having
+double ln(double x) {
+    double old_sum = 0.0;
+    double xmlxpl = (x - 1) / (x + 1);
+    double xmlxpl_2 = xmlxpl * xmlxpl;
+    double denom = 1.0;
+    double frac = xmlxpl;
+    double term = frac; // denom start from 1.0
+    double sum = term;
+
+    while (sum != old_sum) {
+        old_sum = sum;
+        denom += 2.0;
+        frac *= xmlxpl_2;
+        sum += frac / denom;
+    }
+    return 2.0 * sum;
+}
+
+double log10(double x) { return ln(x) / 2.3025850929940456840179914546844; }
--- cstdlib/cmath.h
+++ cstdlib/cmath.h
@@ -5,3 +5,6 @@ float sqrt(float arg);
 
 float powf(float base, int exponent);
 double pow(double base, int exponent);
+
+double ln(double x);
+double log10( double x );
