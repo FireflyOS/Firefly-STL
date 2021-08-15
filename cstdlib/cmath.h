ff193f43e107c3ec3c72808970ee2fe6a8cab314
Yuhanun Citgez <yuhanun.c@gmail.com>
2021-08-15 20:06:29 +0000

    Added math functions + std::swap and move

--- cstdlib/cmath.h
+++ cstdlib/cmath.h
@@ -1,10 +1,11 @@
 #pragma once
-
+#include "cstdint.h"
 
 float sqrt(float arg);
 
 float powf(float base, int exponent);
-double pow(double base, int exponent);
+size_t pow(size_t base, size_t exponent);
 
 double ln(double x);
+int log2(uint64_t x);
 double log10( double x );
