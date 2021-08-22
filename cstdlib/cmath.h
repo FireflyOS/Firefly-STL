edc3b8ba2a3ecbb6704b15da22589a607da0121b
Yuhanun Citgez <Yuhanun.c@gmail.com>
2021-08-22 12:31:20 +0000

    Added constexpr_log2

--- cstdlib/cmath.h
+++ cstdlib/cmath.h
@@ -9,3 +9,7 @@ size_t pow(size_t base, size_t exponent);
 double ln(double x);
 int log2(uint64_t value);
 double log10( double x );
+
+constexpr inline size_t constexpr_log2(size_t n) {
+    return ((n < 2) ? 1 : 1 + constexpr_log2(n / 2));
+}
